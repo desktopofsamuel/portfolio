772455b8fd61675eec160bf776ea1f92ba3e8cf1
Samuel PC <sammatwong@gmail.com>
2019-04-06 12:20:34 +0800

    New Nav Bar

--- src/components/Navigation/navigation.jsx
+++ src/components/Navigation/navigation.jsx
@@ -1,113 +1,64 @@
 import React, { Component } from "react";
 import { Link } from "gatsby";
 import styled from "styled-components";
-import { FaEnvelope } from "react-icons/fa";
-import Layout from "../../layout/";
+import "../../layout/index.css"
+import { FaDribbble, FaTwitter, FaInstagram } from "react-icons/fa";
 
-const Main = styled.div`
-  display: grid;
-  grid-template-columns: minmax(0.5vw,10vw) [content] auto minmax(0.5vw,10vw);
-  margin: 5vh auto 0 auto;
-`;
+const LeftSidebar = styled.aside`
+position: fixed;
+height: 100%;
+width: 50px;
+right: auto;
+bottom: 0;
+display: flex;
+background-color: var(--color-white-700);
+z-index: 1000;
+padding: 8px;
+justify-content: center;
 
-const Content = styled.div`
-  grid-area: content;
-  width: 100%;
-`;
-
-const Title = styled.div`
-  float: left;
-  padding: var(--padding-m) 0;
-  height: 10vh;
-`;
-
-const Contact = styled.button`
-  background: none;
-  text-align: center;
-  box-shadow: none;
-  border: none;
-  float: right;
-  height: 10vh;
-  padding: var(--padding-m);
-  transition: all 0.2s ease-in-out;
-  margin-right: -2rem;
-  
-  a > * {
-    margin-bottom: 0;
-    border-bottom: none;
-  }
-
-  .nav {
-    color: var(--color-black-500);
-  }
-  
-  &:hover {
-    background: var(--color-black-500);
-    a > * {
-    color: var(--color-white-500);
-    }
-  }
-
-  .with-icon {
-    margin-right: 0.5rem;
-  }
-
-  @media screen and (max-width: 1080px) {
-    visibility: visible;
-    opacity: 1;
-  }
-`;
-
-const Bar = styled.div`
-  padding: var(--padding-m) 0 var(--padding-m) var(--padding-m);
-  display: grid;
-  grid-auto-flow: column;
-  grid-gap: var(--padding-l);
-  background-color: var(--color-black-500);
-  float: right;
-  align-items: center;
-  justify-content: center;
-
-  a {
-    border-bottom: none;
-  }
-
-  @media screen and (max-width: 1080px) {
-    visibility: hidden;
-    opacity: 0;
-    display: none;
-  }
+@media only screen and (max-width: 768px) {
+  display: none;
+}
 `
 
-const Separator = styled.div`
-  width: 200px;
-  height: 1px;
-  background-color: var(--color-white-500);
-  align-self: center;
-  padding-right: 0;
-  margin-right: 0;
+const IconWrapper = styled.div`
+bottom: 0;
+display: grid;
+grid-gap: 16px;
+align-content: flex-end;
+`
+
+const NavBar = styled.div`
+margin-left: 50px;
+padding: var(--padding-m);
+
+@media only screen and (max-width: 768px) {
+  margin-left: 0px;
+}
+`
+
+const SiteTitle = styled.h2 `
+font-family: var(--primary-font);
+font-weight: 700;
+
 `
 
 export default class Navigation extends React.Component {
   render() {
     return (
-      <Main>
-        <Content>
-          <Title>
-            <h5><Link to="/">Samuel W.</Link></h5>
-          </Title>
-          {/*<Bar>
-            <Link to="/blog"><h5 className="nav">Blog</h5></Link>
-            <a href="https://pin.desktopofsamuel.com/"><h5 className="nav">Pins</h5></a>
-            <a href="https://photo.desktopofsamuel.com/"><h5 className="nav">Photos</h5></a>
-            <a href="mailto:desktopofsamuel@gmail.com" ><h5 className="nav">Contact</h5></a>
-            <Separator/>
-          </Bar>*/}
-          <Contact>
-          <a href="mailto:desktopofsamuel@gmail.com" ><h5 className="nav"><FaEnvelope className="with-icon"/>Contact</h5></a>
-          </Contact>
-        </Content>
-      </Main>
-    );
-  }
-}
+      <div>
+        <LeftSidebar>
+          <IconWrapper>
+            <a class="noeffect linkicon" href="/"><FaDribbble size="1.25rem"/></a>
+            <a class="noeffect linkicon" href="/"><FaTwitter  size="1.25rem"/></a>
+            <a class="noeffect linkicon" href="/"><FaInstagram  size="1.25rem"/></a>
+          </IconWrapper>
+        </LeftSidebar>
+        <NavBar>
+        <SiteTitle><Link to="/">Samuel Wong</Link> - Product Designer</SiteTitle>
+        </NavBar>
+        </div>
+            );
+          }
+        }
+        
