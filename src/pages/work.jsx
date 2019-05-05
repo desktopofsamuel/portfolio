42c747aafc00c62a3c37cf1b744d3b8649f80d55
Samuel PC <sammatwong@gmail.com>
2019-05-06 02:18:30 +0800

    Fix Work Page Link Error

--- src/pages/work.jsx
+++ src/pages/work.jsx
@@ -3,18 +3,48 @@ import Helmet from "react-helmet";
 import { graphql } from "gatsby";
 import Layout from "../layout";
 import PostListing from "../components/PostListing/PostListing";
+import WorkHero from "../components/WorkHero/WorkHero";
 import SEO from "../components/SEO/SEO";
 import config from "../../data/SiteConfig";
+import styled from "styled-components";
+
+const Row = styled.section`
+padding: var(--var-padding-l) 0;
+background: white; 
+
+&:first-child {
+  padding-bottom: 0;
+}
+`
+
+const BoxContent = styled.div`
+max-width: 85vw;
+padding-left: 50px;
+margin: 0 auto;
+
+  @media only screen and (max-width: 1024px) {
+    max-width: 95vw;
+    padding: 0 1.5rem;
+  }
+
+  @media only screen and (max-width: 768px) {
+    padding: 0 1rem;
+}
+`
 
 class Index extends React.Component {
   render() {
     const postEdges = this.props.data.allMarkdownRemark.edges;
     return (
       <Layout>
-        <div className="index-container">
+          <Row>
+              <small>Projects</small>
+              <h1>My Work</h1>
+          </Row>
+          <Row>
           <Helmet title={config.siteTitle} />
-          <PostListing invert postEdges={postEdges} />
-        </div>
+          <WorkHero postEdges={postEdges} />
+          </Row>
       </Layout>
     );
   }
@@ -40,7 +70,9 @@ export const pageQuery = graphql`
           timeToRead
           frontmatter {
             title
+            subtitle
             tags
+            color
             cover {
               publicURL
               size
