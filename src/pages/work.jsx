bd6a115c0ccfd989b636b2538a29eb08d01ea0b8
desktopofsamuel <you@example.com>
2019-07-26 07:37:40 +0800

    Update dependency 26/7/2019

--- src/pages/work.jsx
+++ src/pages/work.jsx
@@ -9,18 +9,17 @@ import config from "../../data/SiteConfig";
 import styled from "styled-components";
 
 const Row = styled.section`
-padding: var(--var-padding-l) 0;
-background: white; 
+  padding: var(--var-padding-l) 0;
 
-&:first-child {
-  padding-bottom: 0;
-}
-`
+  &:first-child {
+    padding-bottom: 0;
+  }
+`;
 
 const BoxContent = styled.div`
-max-width: 85vw;
-padding-left: 50px;
-margin: 0 auto;
+  max-width: 85vw;
+  padding-left: 50px;
+  margin: 0 auto;
 
   @media only screen and (max-width: 1024px) {
     max-width: 95vw;
@@ -29,22 +28,22 @@ margin: 0 auto;
 
   @media only screen and (max-width: 768px) {
     padding: 0 1rem;
-}
-`
+  }
+`;
 
 class Index extends React.Component {
   render() {
     const postEdges = this.props.data.allMarkdownRemark.edges;
     return (
       <Layout>
-          <Row>
-              <small>Projects</small>
-              <h1>My Work</h1>
-          </Row>
-          <Row>
+        <Row>
+          <small>Projects</small>
+          <h1>My Work</h1>
+        </Row>
+        <Row>
           <Helmet title={config.siteTitle} />
           <WorkHero postEdges={postEdges} />
-          </Row>
+        </Row>
       </Layout>
     );
   }
@@ -58,7 +57,7 @@ export const pageQuery = graphql`
     allMarkdownRemark(
       limit: 2000
       sort: { fields: [fields___date], order: DESC }
-      filter: {fileAbsolutePath: {regex: "/work/"}}
+      filter: { fileAbsolutePath: { regex: "/work/" } }
     ) {
       edges {
         node {
