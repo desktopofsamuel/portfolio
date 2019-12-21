c8753b501828858d223af5fc38ea04ab60a8c0ee
Samuel Wong <sammatwong@gmail.com>
2019-12-21 23:41:56 +0800

    Wrong Branch, Update on CSS

--- src/pages/work.jsx
+++ src/pages/work.jsx
@@ -2,7 +2,14 @@ import React from "react";
 import Helmet from "react-helmet";
 import { graphql } from "gatsby";
 import Layout from "../layout";
+import Link from "../components/GatsbyLink/GatsbyLink";
 import WorkHero from "../components/WorkHero/WorkHero";
+import WaterSVG from "../../static/SVG/Water.svg";
+import BookSVG from "../../static/SVG/Book.svg";
+import HyperAirSVG from "../../static/SVG/HyperAir-2.svg";
+import PinSVG from "../../static/SVG/Pin.svg";
+import PlayaSVG from "../../static/SVG/Playa.svg";
+import PingspaceSVG from "../../static/SVG/Pingspace.svg";
 import SEO from "../components/SEO/SEO";
 import config from "../../data/SiteConfig";
 import styled from "styled-components";
@@ -31,6 +38,72 @@ const BoxContent = styled.div`
   }
 `;
 
+const GridProject = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(260px, 1fr));
+  grid-gap: 1rem;
+`;
+
+const PassionPart = styled.div``;
+const PassionContent = styled.div``;
+
+const ProjectWrapper = styled.div`
+  background: var(--color-white-700);
+  align-content: space-between;
+  padding: 2rem;
+  display: flex;
+  flex-flow: column;
+  transition: transform 0.2s ease-in, box-shadow 0.3s ease-in-out;
+
+  &:hover {
+    transform: scale(1.025, 1.025);
+    box-shadow: 0px 1px 4px rgba(0, 0, 0, 0.1);
+  }
+`;
+
+const ProjectContent = styled.div``;
+
+const ProjectBottom = styled.div`
+  display: flex;
+  align-items: baseline;
+  justify-content: space-between;
+  justify-self: flex-end;
+`;
+
+const ProjectIcon = styled.div`
+  margin-bottom: 2rem;
+`;
+
+const ProjectTitle = styled.h3``;
+
+const ProjectBlurb = styled.p`
+  font-size: 0.975rem;
+`;
+
+const ProjectYear = styled.small`
+  margin-bottom: 0;
+`;
+
+const ProjectLink = styled(Link)``;
+
+const ProjectBox = props => (
+  <ProjectWrapper>
+    <ProjectIcon>
+      <img src={props.img} width="50px" />
+    </ProjectIcon>
+    <ProjectContent>
+      <ProjectTitle>{props.title}</ProjectTitle>
+      <ProjectBlurb>{props.blurb}</ProjectBlurb>
+    </ProjectContent>
+    <ProjectBottom>
+      <ProjectYear>{props.year}</ProjectYear>
+      <ProjectLink className="noeffect" to={props.url} target="blank">
+        ↗
+      </ProjectLink>
+    </ProjectBottom>
+  </ProjectWrapper>
+);
+
 class Index extends React.Component {
   render() {
     const postEdges = this.props.data.allMdx.edges;
@@ -44,6 +117,57 @@ class Index extends React.Component {
           <Row>
             <Helmet title={config.siteTitle} />
             <WorkHero postEdges={postEdges} />
+            <GridProject>
+              <ProjectBox
+                img={HyperAirSVG}
+                title="HyperAir "
+                blurb="Travel platform website and app that I've worked full-time."
+                year="2019"
+                url="https://www.hyperair.com"
+              />
+              <ProjectBox
+                img={PinSVG}
+                title="Pins"
+                blurb="Curated design resource site coded by myself using GatsbyJS."
+                year="2018"
+                url="https://pins.desktopofsamuel.com"
+              />
+              {/* <ProjectBox
+                  img={DocuSVG}
+                  title="Road Not Taken"
+                  blurb="A documentary I directed with multiple Asian film festival selected."
+                  year="2016"
+                  url="https://vimeo.com/ondemand/roadnottaken"
+                /> */}
+              <ProjectBox
+                img={PingspaceSVG}
+                title="Pingspace"
+                blurb="Websites uptime monitor as a personal project"
+                year="2018"
+                url="https://pingspace.webflow.io/"
+              />
+              <ProjectBox
+                img={PlayaSVG}
+                title="Playa"
+                blurb="Revamped portfolio and landing page of my agency"
+                year="2018"
+                url="https://playa.hk/portfolio.html"
+              />
+              <ProjectBox
+                img={WaterSVG}
+                title="Water For Free"
+                blurb="First crowdsourcing water dispenser map in Hong kong"
+                year="2017"
+                url="https://waterforfree.org/zh/%E4%B8%8B%E8%BC%89%E6%87%89%E7%94%A8/"
+              />
+              <ProjectBox
+                img={BookSVG}
+                title="Creation Cabin"
+                blurb="Online novel platform supported by an independent publisher"
+                year="2017"
+                url="https://playa.hk/projects/creation-cabin-reading-platform.html"
+              />
+            </GridProject>
           </Row>
         </Boxed>
       </Layout>
