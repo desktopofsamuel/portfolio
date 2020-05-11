1531f56b2c1d8dd82776ea9eba477f16bd03f1d6
Samuel W <sammatwong@gmail.com>
2020-05-11 13:16:19 +0800

    Edit HyperAir Content

--- src/elements/GridBlock.jsx
+++ src/elements/GridBlock.jsx
@@ -4,7 +4,8 @@ import styled from "styled-components";
 const Grid = styled.div`
   display: grid;
   grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
-  grid-gap: var(--var-padding-m);
+  grid-gap: var(--var-padding-s);
+  margin: var(--var-padding-m) 0;
 `;
 
 const GridBlock = ({ children, className }) => (
