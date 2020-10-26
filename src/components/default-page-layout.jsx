9b0bc17e964c496295a8e5ae6e41bc662c1d58d7
Samuel Wong <35249407+desktopofsamuel@users.noreply.github.com>
2020-10-27 01:31:27 +0800

    Add Sidebar to Default PAge

--- src/components/default-page-layout.jsx
+++ src/components/default-page-layout.jsx
@@ -6,6 +6,20 @@ import Layout from "../layout";
 import { Helmet } from "react-helmet";
 import config from "../../data/SiteConfig";
 
+const Main = styled.main``;
+
+const Sidebar = styled.aside``;
+
+const Container = styled(Boxed)`
+  display: grid;
+  grid-template-columns: 7fr 3fr;
+  gap: 2rem;
+
+  @media only screen and (max-width: 768px) {
+    display: block;
+  }
+`;
+
 export default class DefaultPageLayout extends Component {
   render() {
     return (
@@ -14,7 +28,12 @@ export default class DefaultPageLayout extends Component {
           <title>{`${this.props.pageContext.frontmatter.title} | ${config.siteTitle} `}</title>
         </Helmet>
         <SEO postPath={this.props.path} postNode={this.props.pageContext} />
-        <Boxed>{this.props.children}</Boxed>
+        <Container>
+          <Main>{this.props.children}</Main>
+          <Sidebar>
+            <p>Hello</p>
+          </Sidebar>
+        </Container>
       </Layout>
     );
   }
