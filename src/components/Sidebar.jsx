8f41fb42ad39578d80d6796b8f9d9f174112f17f
Samuel W <sammatwong@gmail.com>
2020-10-27 20:49:52 +0800

    Create Sidebar Component

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.aside``;
+
+const Sidebar = () => {
+  return <Wrapper></Wrapper>;
+};
+
+export default Sidebar;
--- src/components/default-page-layout.jsx
+++ src/components/default-page-layout.jsx
@@ -5,10 +5,9 @@ import Boxed from "elements/Boxed";
 import Layout from "../layout";
 import { Helmet } from "react-helmet";
 import config from "../../data/SiteConfig";
+import Sidebar from "./sidebar";
 
 const Main = styled.main``;
-
-const Sidebar = styled.aside``;
 
 const Container = styled(Boxed)`
   display: grid;
@@ -30,9 +29,7 @@ export default class DefaultPageLayout extends Component {
         <SEO postPath={this.props.path} postNode={this.props.pageContext} />
         <Container>
           <Main>{this.props.children}</Main>
-          <Sidebar>
-            <p>Hello</p>
-          </Sidebar>
+          <Sidebar />
         </Container>
       </Layout>
     );
