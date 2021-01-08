fe2a338a9519492978c5e066c9e2bc8da14f6f36
Samuel Wong <35249407+desktopofsamuel@users.noreply.github.com>
2021-01-09 01:14:19 +0800

    Revert "A11y: Update to Accessible Color for color-primary"
    This reverts commit 9d25f834a974f85a85cf7121016505ee0c1196d0.

--- src/components/page/IndexHero.jsx
+++ src/components/page/IndexHero.jsx
@@ -90,7 +90,11 @@ const IndexHero = () => {
             I co-founded Playa, a web & app agency. Before that, I worked as
             Cross-Content Intern at iTunes & App Store, Apple.
           </Subtitle>
-          <ReadOn text="View My Work" href="/work" target="self" />
+          <ReadOn
+            text="View My Work"
+            href="https://apple.com"
+            target="_blank"
+          />
         </IntroWrapper>
         {/* <LinkWrapper>
           <small>Contact</small>
