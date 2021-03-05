6586d4308af8bbb75bcb9dd7d5113e370de712d3
Samuel Wong <35249407+desktopofsamuel@users.noreply.github.com>
2021-03-05 15:51:19 +0800

    Fix Build Error

--- gatsby-config.js
+++ gatsby-config.js
@@ -190,7 +190,7 @@ module.exports = {
         name: config.siteTitle,
         short_name: config.siteTitleShort,
         description: config.siteDescription,
-        start_url: config.pathPrefix,
+        start_url: "/",
         background_color: config.backgroundColor,
         theme_color: config.themeColor,
         display: "minimal-ui",
