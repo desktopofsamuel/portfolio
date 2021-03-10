a07f3d920b2ada0f66013c2a641303a30f292bd9
Samuel Wong <35249407+desktopofsamuel@users.noreply.github.com>
2021-03-10 09:30:35 +0800

    Update Resources page

--- src/pages/resources.jsx
+++ src/pages/resources.jsx
@@ -6,6 +6,10 @@ import { H2, BodyMain } from "components/common/TextStyles";
 import ReadOn from "components/ReadOn";
 import Layout from "../layout";
 import Resource1 from "../../static/SVG/Resource.svg";
+import UXDesignCareerKit from "../../static/images/20210310-UXDesignCareerKit.png";
+import Apps from "../../static/images/20210310-appsservices.png";
+import Whatiuse from "../../static/images/20210310-whatiuse.png";
+import DesignOKR from "../../content/blog/2021-03-09 Design OKR/20210309-DesignOKR.png";
 
 const Grid = styled.div`
   display: grid;
@@ -35,16 +39,33 @@ const ResourcePage = () => {
       <Boxed>
         <Grid>
           <Wrapper>
-            <img src={Resource1} alt="Design Career Kit" />
+            <img src={DesignOKR} alt="Design OKR Template" />
           </Wrapper>
           <Wrapper>
-            <Title>Design Career Kit</Title>
-            <BodyMain>My list of favorite and helpful resources</BodyMain>
+            <Title>Design OKR</Title>
+            <BodyMain>
+              OKR template to get you started to progress your design career!
+              With actionable objectives and key results.
+            </BodyMain>
+            <ReadOn
+              text="Read How To Use"
+              href="/navigating-design-career-with-design-okrs/"
+            />
+          </Wrapper>
+          <Wrapper>
+            <img src={UXDesignCareerKit} alt="UX Design Career Kit" />
+          </Wrapper>
+          <Wrapper>
+            <Title>UX Design Career Kit</Title>
+            <BodyMain>
+              My list of favorite and helpful resources for job hunting. Helpful
+              tips from Twitter's design leaders are included!
+            </BodyMain>
             <ReadOn text="Get The Kit" href="/ux-design-career-kit/" />
           </Wrapper>
           <Wrapper>
             {" "}
-            <img src={Resource1} alt="Design Career Kit" />
+            <img src={Apps} alt="Apps & Services" />
           </Wrapper>
           <Wrapper>
             <H2>Apps & Services</H2>
@@ -56,7 +77,7 @@ const ResourcePage = () => {
           </Wrapper>
           <Wrapper>
             {" "}
-            <img src={Resource1} alt="Design Career Kit" />
+            <img src={Whatiuse} alt="What I Use" />
           </Wrapper>
           <Wrapper>
             <H2>What I Use</H2>
