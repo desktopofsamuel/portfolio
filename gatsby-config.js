21b3919dac2987ecd7b2e907f6bff9494ccbfa38
Samuel Wong <35249407+desktopofsamuel@users.noreply.github.com>
2021-03-23 01:23:49 +0800

    Chore: Remove unnecessary package

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,4 @@
 const urljoin = require("url-join");
-const path = require("path");
 const config = require("./data/SiteConfig");
 
 require("dotenv").config({
@@ -202,16 +201,6 @@ module.exports = {
         icon: "src/favicon.png",
       },
     },
-    /* {
-      resolve: `gatsby-source-cloudinary`,
-      options: {
-        cloudName: process.env.CLOUDINARY_CLOUD_NAME,
-        apiKey: process.env.CLOUDINARY_API_KEY,
-        apiSecret: process.env.CLOUDINARY_API_SECRET,
-        resourceType: `image`,
-        tags: true
-      }
-    }, */
     // "gatsby-plugin-webpack-bundle-analyzer",
     {
       resolve: `gatsby-plugin-webfonts`,
@@ -264,83 +253,83 @@ module.exports = {
         },
       },
     },
-    // {
-    //   resolve: "gatsby-plugin-feed",
-    //   options: {
-    //     setup(ref) {
-    //       const ret = ref.query.site.siteMetadata.rssMetadata;
-    //       ret.allMdx = ref.query.allMdx;
-    //       ret.generator = "GatsbyJS Material Starter";
-    //       return ret;
-    //     },
-    //     query: `
-    //     {
-    //       site {
-    //         siteMetadata {
-    //           rssMetadata {
-    //             site_url
-    //             feed_url
-    //             title
-    //             description
-    //             image_url
-    //             author
-    //             copyright
-    //           }
-    //         }
-    //       }
-    //     }
-    //   `,
-    //     feeds: [
-    //       {
-    //         serialize(ctx) {
-    //           const { rssMetadata } = ctx.query.site.siteMetadata;
-    //           return ctx.query.allMdx.edges.map(edge => ({
-    //             categories: edge.node.frontmatter.tags,
-    //             date: edge.node.fields.date,
-    //             title: edge.node.frontmatter.title,
-    //             description: edge.node.excerpt,
-    //             author: rssMetadata.author,
-    //             url: rssMetadata.site_url + edge.node.fields.slug,
-    //             guid: rssMetadata.site_url + edge.node.fields.slug,
-    //             custom_elements: [
-    //               {
-    //                 "content:encoded": edge.node.html,
-    //               },
-    //             ],
-    //           }));
-    //         },
-    //         query: `
-    //         {
-    //           allMdx(
-    //             limit: 1000,
-    //             sort: { order: DESC, fields: [fields___date] },
-    //           ) {
-    //             edges {
-    //               node {
-    //                 excerpt
-    //                 html
-    //                 fields {
-    //                   slug
-    //                   date
-    //                 }
-    //                 frontmatter {
-    //                   title
-    //                   cover {
-    //                     publicURL
-    //                   }
-    //                   date
-    //                   category
-    //                   tags
-    //                 }
-    //               }
-    //             }
-    //           }
-    //         }
-    //       `,
-    //         output: config.siteRss,
-    //       },
-    //     ],
-    //   },
-    // },
+    {
+      resolve: "gatsby-plugin-feed",
+      options: {
+        setup(ref) {
+          const ret = ref.query.site.siteMetadata.rssMetadata;
+          ret.allMdx = ref.query.allMdx;
+          ret.generator = "GatsbyJS Material Starter";
+          return ret;
+        },
+        query: `
+        {
+          site {
+            siteMetadata {
+              rssMetadata {
+                site_url
+                feed_url
+                title
+                description
+                image_url
+                author
+                copyright
+              }
+            }
+          }
+        }
+      `,
+        feeds: [
+          {
+            serialize(ctx) {
+              const { rssMetadata } = ctx.query.site.siteMetadata;
+              return ctx.query.allMdx.edges.map(edge => ({
+                categories: edge.node.frontmatter.tags,
+                date: edge.node.fields.date,
+                title: edge.node.frontmatter.title,
+                description: edge.node.excerpt,
+                author: rssMetadata.author,
+                url: rssMetadata.site_url + edge.node.fields.slug,
+                guid: rssMetadata.site_url + edge.node.fields.slug,
+                custom_elements: [
+                  {
+                    "content:encoded": edge.node.html,
+                  },
+                ],
+              }));
+            },
+            query: `
+            {
+              allMdx(
+                limit: 1000,
+                sort: { order: DESC, fields: [fields___date] },
+              ) {
+                edges {
+                  node {
+                    excerpt
+                    html
+                    fields {
+                      slug
+                      date
+                    }
+                    frontmatter {
+                      title
+                      cover {
+                        publicURL
+                      }
+                      date
+                      category
+                      tags
+                    }
+                  }
+                }
+              }
+            }
+          `,
+            output: config.siteRss,
+          },
+        ],
+      },
+    },
   ],
 };
