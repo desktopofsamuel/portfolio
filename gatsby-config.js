f6f2715bb88773f2f7bb0a5c9b8cad45a3d90981
Samuel Wong <35249407+desktopofsamuel@users.noreply.github.com>
2021-04-01 21:19:37 +0800

    Fixed Gatsby v3 RSS issue

--- gatsby-config.js
+++ gatsby-config.js
@@ -305,8 +305,7 @@ module.exports = {
                 author: rssMetadata.author,
                 url: rssMetadata.site_url + edge.node.fields.slug,
                 guid: rssMetadata.site_url + edge.node.fields.slug,
-                // custom_elements: [{ "content:encoded": edge.node.html }],
-                custom_elements: edge.node.html,
+                custom_elements: [{ "content:encoded": edge.node.html }],
               }));
             },
             query: `
